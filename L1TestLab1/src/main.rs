387e9b644575a853a5148eac3d4d21b2288cc087
Davidsenec <sgd88.thomponsenec@yahoo.com>
2025-07-12 21:25:46 +0700

    Added a bunch of files, this took way too fucking long

---
+++ L1TestLab1/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let sr = 1.0;
+    let cyr = 1.0;
+    let cor = 1.0;
+    let cyh = 1.0;
+    let coh = 1.0;
+
+    let vols = ;
+    let volcy = ;
+    let volco = ;
+
+    println!("The volume of the sphere is {vols}");
+    println!("The volume of the cylinder is {volcy}");
+    println!("The volume of the cone is {volco}");
+
+}
---
+++ L1setup/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let x = "Open your terminal (CTRL + SHIFT + `)\n\
+    Type cd *Insert folder name here*\n\
+    If you're creating a new file, type *cargo new file_name*\n\
+    Then type *cargo run*";
+    
+    println!("Hello, world!");
+    println!("The purpose of this file is to teach the user how to setup Rust");
+
+    println!("{x}");
+    
+}

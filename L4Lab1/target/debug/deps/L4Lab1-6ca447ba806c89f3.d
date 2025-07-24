85b6906aae5adf2b8a8ad201c3aa4bb90382cb9a
Davidsenec <sgd88.thomponsenec@yahoo.com>
2025-07-24 14:06:18 +0700

    renamed most variables in Lab according to the standard I think

---
+++ L4Lab1/target/debug/deps/L4Lab1-6ca447ba806c89f3.d
@@ -0,0 +1,5 @@
+C:\code\Rust\L4Lab1\target\debug\deps\libL4Lab1-6ca447ba806c89f3.rmeta: src\main.rs
+
+C:\code\Rust\L4Lab1\target\debug\deps\L4Lab1-6ca447ba806c89f3.d: src\main.rs
+
+src\main.rs:
--- L4Lab2/src/main.rs
+++ L4Lab2/src/main.rs
@@ -1,7 +1,7 @@
 use std::io::{self};
 
 fn main() {
-    let mut warehouse:Vec<(u32, String, u32)> = Vec::new();
+    let mut warehouse:Vec<(u32, String, u32)> = Vec::new(); //creating
 
     loop {
         println!("Warehouse Inventory Management:");
@@ -18,40 +18,40 @@ fn main() {
         match choice.trim() {
         "1" => {
             println!("Enter new product ID: ");
-            let mut newID = String::new();
-            io::stdin().read_line(&mut newID).expect("Failed to read input");
-            let newID:u32 = newID.trim().parse().expect("Invalid ID");
+            let mut new_id = String::new();
+            io::stdin().read_line(&mut new_id).expect("Failed to read input");
+            let new_id:u32 = new_id.trim().parse().expect("Invalid ID");
 
-            let mut newName = String::new();
+            let mut new_name = String::new();
             println!("Enter new product name: ");
-            io::stdin().read_line(&mut newName).expect("Failed to read input");
-            let newName = newName.trim().to_string();
+            io::stdin().read_line(&mut new_name).expect("Failed to read input");
+            let new_name = new_name.trim().to_string();
             
-            let mut newQuantity = String::new();
+            let mut new_quantity = String::new();
             println!("Enter quantity: ");
-            io::stdin().read_line(&mut newQuantity).expect("Failed to read input");
-            let newQuantity:u32 = newQuantity.trim().parse().expect("Invalid quantity");
+            io::stdin().read_line(&mut new_quantity).expect("Failed to read input");
+            let new_quantity:u32 = new_quantity.trim().parse().expect("Invalid quantity");
 
-            warehouse.push((newID, newName, newQuantity));
+            warehouse.push((new_id, new_name, new_quantity));
             println!("Added new product");
         }
 
         "2" => {
             println!("Enter ID desired product to update quantity: ");
-            let mut ID = String::new();
-            io::stdin().read_line(&mut ID).expect("Failed to read input");
-            let ID:u32 = ID.trim().parse().expect("Invalid ID");
+            let mut id = String::new();
+            io::stdin().read_line(&mut id).expect("Failed to read input");
+            let id:u32 = id.trim().parse().expect("Invalid ID");
             let mut i = 0;
             let mut id_exists = false;
-            for (IDP, _, _) in &warehouse {
-                if *IDP == ID {
-                    let mut newQuantity = String::new();
+            for (idp, _, _) in &warehouse {
+                if *idp == id {
+                    let mut new_quantity = String::new();
                     println!("Enter quantity: ");
-                    io::stdin().read_line(&mut newQuantity).expect("Failed to read input");
-                    let newQuantity:u32 = newQuantity.trim().parse().expect("Invalid quantity");
+                    io::stdin().read_line(&mut new_quantity).expect("Failed to read input");
+                    let new_quantity:u32 = new_quantity.trim().parse().expect("Invalid quantity");
                     id_exists = true;
-                    warehouse[i].2 = newQuantity; 
-                    println!("Removed item {ID}");
+                    warehouse[i].2 = new_quantity; 
+                    println!("Removed item {id}");
                     break;
                 } else {
                     println!("ID not found")
@@ -61,17 +61,17 @@ fn main() {
         }
         "3" => {
             println!("Enter ID of desired product of removal: ");
-            let mut ID = String::new();
-            io::stdin().read_line(&mut ID).expect("Failed to read input");
-            let ID:u32 = ID.trim().parse().expect("Invalid ID");
+            let mut id = String::new();
+            io::stdin().read_line(&mut id).expect("Failed to read input");
+            let id:u32 = id.trim().parse().expect("Invalid ID");
             
             let mut i = 0;
             let mut id_exists = false;
-            for (IDP, _, _) in &warehouse {
-                if *IDP == ID {
+            for (idp, _, _) in &warehouse {
+                if *idp == id {
                     id_exists = true;
                     warehouse.remove(i);
-                    println!("Removed item {ID}");
+                    println!("Removed item {id}");
                     break;
                 } else {
                     println!("ID not found")

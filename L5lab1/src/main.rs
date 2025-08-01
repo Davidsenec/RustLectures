8c33726b588120a28ecebe50befb4bccc357427a
Davidsenec <sgd88.thomponsenec@yahoo.com>
2025-08-01 23:12:30 +0700

    added 2 new functions to temp checker

--- L5lab1/src/main.rs
+++ L5lab1/src/main.rs
@@ -8,6 +8,18 @@ fn ftc(f:f64) -> f64 {
     (f - 32.0) * 5.0/9.0
 }
 
+fn show_temp(label: &str, value: &f64) {
+    println!("{} temperature: {:.2} degrees {}", label, value, label)
+}
+
+fn adjust_temp(value: &f64, delta: f64) {
+    let mut value = *value;
+    value += delta;
+    println!("Adjusted value: {}", value);
+
+}
+
+
 fn main() {
     println!("1. Celcius -> Fahrenheit");
     println!("2. Fahrenheit -> Celcius");
@@ -15,7 +27,7 @@ fn main() {
     let mut choice = String::new();
     print!("Enter your choice: ");
     io::stdout().flush().unwrap();
-    io::stdin().read_line( &mut choice).expect("Invalid input");
+    io::stdin().read_line(&mut choice).expect("Invalid input");
     
     match choice.trim() {
         "1" => {
@@ -23,8 +35,11 @@ fn main() {
             print!("Enter celius temp: ");
             io::stdout().flush().unwrap();
             io::stdin().read_line(&mut cel).expect("Invalid input");
-            let cel:f64 = cel.trim().parse().expect("Invalid temp value");
-            println!("{} degrees fahrenheit", ctf(cel));
+            let mut cel:f64 = cel.trim().parse().expect("Invalid temp value");
+            cel = ctf(cel);
+            let fah = "Fahrenheit";
+            show_temp(&fah, &cel);
+            adjust_temp(&cel, 0.5);
 
         }
         "2" => {
@@ -32,8 +47,11 @@ fn main() {
             print!("Enter fahrenheit temp: ");
             io::stdout().flush().unwrap();
             io::stdin().read_line(&mut fah).expect("Invalid input");
-            let fah:f64 = fah.trim().parse().expect("Invalid temp value");
-            println!("{} degrees celcius", ftc(fah));
+            let mut fah:f64 = fah.trim().parse().expect("Invalid temp value");
+            fah = ftc(fah);
+            let cel = "Celcius";
+            show_temp(&cel, &fah);
+            adjust_temp(&fah, 0.5);
         }
         _ => {
             println!("Invalid input");
